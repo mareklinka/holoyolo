7deb5d0c5dd96a25abc31860bce2ba77f8636f0f
Marek Linka <marek.linka@outlook.com>
2018-06-19 17:10:57 +0200

    Add media frame capture

--- src/unity/unity-emotion-demo/Assets/Scripts/ImageToEmotionAPI.cs
+++ src/unity/unity-emotion-demo/Assets/Scripts/ImageToEmotionAPI.cs
@@ -1,57 +1,103 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
-using UnityEngine.Networking;
-using System;
-using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using HoloLensCameraStream;
+using Debug = UnityEngine.Debug;
 
-public class ImageToEmotionAPI : MonoBehaviour {
+public class ImageToEmotionAPI : MonoBehaviour
+{
+    private VideoCapture capture;
 
-    string EMOTIONKEY = "YOURKEYHERE"; // replace with your Emotion API Key
+    public string LatestFrame { get; private set; }
 
-    string emotionURL = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+    private bool captureStarted;
 
-    public string fileName { get; private set; }
-    string responseData;
+    private int iterations = 0;
+    private TimeSpan duration = TimeSpan.Zero;
 
-    // Use this for initialization
-    void Start () {
-	    fileName = Path.Combine(Application.streamingAssetsPath, "myphoto.jpeg"); // Replace with your file
+    void Start()
+    {
+        VideoCapture.CreateAync(OnCreatedCallback);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-        // This will be called with your specific input mechanism
-        if(Input.GetKeyDown(KeyCode.Space))
+
+    private void OnCreatedCallback(VideoCapture captureobject)
+    {
+        capture = captureobject;
+
+        capture.StartVideoModeAsync(
+            new CameraParameters(cameraResolutionWidth: 640, cameraResolutionHeight: 480, pixelFormat: CapturePixelFormat.NV12), OnVideoModeStartedCallback);
+    }
+
+    private void OnVideoModeStartedCallback(VideoCaptureResult result)
+    {
+        captureStarted = true;
+
+        Task.Run(() => ProcessCamera());
+    }
+
+    private void ProcessCamera()
+    {
+        while (true)
         {
-            StartCoroutine(GetEmotionFromImages());
+            capture.RequestNextFrameSample(OnFrameSampleAcquired);
+        }
+    }
+
+    private void OnFrameSampleAcquired(VideoCaptureSample videocapturesample)
+    {
+        if (videocapturesample == null)
+        {
+            return;
         }
 
-	}
-    /// <summary>
-    /// Get emotion data from the Cognitive Services Emotion API
-    /// Stores the response into the responseData string
-    /// </summary>
-    /// <returns> IEnumerator - needs to be called in a Coroutine </returns>
-    IEnumerator GetEmotionFromImages()
+        var sw = Stopwatch.StartNew();
+        var data = new byte[videocapturesample.dataLength];
+        videocapturesample.CopyRawImageDataIntoBuffer(data);
+
+        DoStuffWithData(data);
+
+        sw.Stop();
+        duration += sw.Elapsed;
+        ++iterations;
+
+        if (iterations % 100 == 0)
+        {
+            Debug.Log("Average time per frame " + duration.TotalMilliseconds / iterations + "ms");
+        }
+    }
+
+    private void DoStuffWithData(byte[] data)
     {
-#if UNITY_WINRT
-        byte[] bytes = UnityEngine.Windows.File.ReadAllBytes(fileName);
-#else
-        byte[] bytes = System.IO.File.ReadAllBytes(fileName);
-#endif
+        Debug.Log("Received framebuffer of length " + data.Length);
 
-        var headers = new Dictionary<string, string>() {
-            { "Ocp-Apim-Subscription-Key", EMOTIONKEY },
-            { "Content-Type", "application/octet-stream" }
-        };
+        LatestFrame = ByteArrayToHexViaLookup32(data);
+    }
 
-        WWW www = new WWW(emotionURL, bytes, headers);
+    private static readonly uint[] _lookup32 = CreateLookup32();
 
-        yield return www;
-        responseData = www.text; // Save the response as JSON string
-        Debug.Log(responseData);
-        GetComponent<ParseEmotionResponse>().ParseJSONData(responseData);
+    private static uint[] CreateLookup32()
+    {
+        var result = new uint[256];
+        for (int i = 0; i < 256; i++)
+        {
+            string s = i.ToString("X2");
+            result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+        }
+        return result;
+    }
+
+    private static string ByteArrayToHexViaLookup32(byte[] bytes)
+    {
+        var lookup32 = _lookup32;
+        var result = new char[bytes.Length * 2];
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            var val = lookup32[bytes[i]];
+            result[2 * i] = (char)val;
+            result[2 * i + 1] = (char)(val >> 16);
+        }
+        return new string(result);
     }
 }
